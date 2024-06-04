7a6b3c3bd5f390209be4f6bae73caef0e8cfda99
RonyAbreu <ronyelison78@gmail.com>
2024-06-04 19:52:23 -0300

    Style: finalizando responsividade do projeto

--- src/routes/checkout/Checkout.tsx
+++ src/routes/checkout/Checkout.tsx
@@ -8,7 +8,7 @@ function Checkout() {
   const { cartItens } = useCart();
   const [subtotal, setSubTotal] = useState<number>(0);
 
-  const {register, handleSubmit, errors} = useValidAddress();
+  const { register, handleSubmit, errors } = useValidAddress();
 
   useEffect(() => {
     let total: number = 0;
@@ -18,44 +18,13 @@ function Checkout() {
     setSubTotal(total);
   }, [cartItens]);
 
-  function finalizeOrder(address : Address){
-    console.log(address)
+  function finalizeOrder(address: Address) {
+    console.log(address);
   }
 
   return (
     <div className={styles.checkout_container}>
       <div className={styles.checkout}>
-        <div className={styles.checkout_column}>
-          <h2 className={styles.title}>Endereço</h2>
-          <form className={styles.address_form}>
-            <label>
-              <p>Rua</p>
-              <input type="text" placeholder="Digite o nome de sua rua" {...register("street")}/>
-              <span>{errors.street?.message}</span>
-            </label>
-            <label>
-              <p>Bairro</p>
-              <input type="text" placeholder="Digite o nome de seu bairro" {...register("burgh")}/>
-              <span>{errors.burgh?.message}</span>
-            </label>
-            <label>
-              <p>Número da Casa</p>
-              <input type="number" min={1} placeholder="Digite o número de sua casa" {...register("number")}/>
-              <span>{errors.number?.message}</span>
-            </label>
-            <label>
-              <p>Cidade</p>
-              <input type="text" placeholder="Digite o nome de sua cidade" {...register("city")}/>
-              <span>{errors.city?.message}</span>
-            </label>
-            <label>
-              <p>Estado</p>
-              <input type="text" placeholder="Digite o nome do seu estado" {...register("state")}/>
-              <span>{errors.state?.message}</span>
-            </label>
-          </form>
-        </div>
-
         <div className={styles.checkout_column}>
           <h2 className={styles.title}>Revise seu Pedido</h2>
           <div className={styles.checkout_items}>
@@ -78,20 +47,72 @@ function Checkout() {
               ))}
           </div>
         </div>
+        
+        <div className={styles.checkout_column}>
+          <h2 className={styles.title}>Endereço</h2>
+          <form className={styles.address_form}>
+            <label>
+              <p>Rua</p>
+              <input
+                type="text"
+                placeholder="Digite o nome de sua rua"
+                {...register("street")}
+              />
+              <span>{errors.street?.message}</span>
+            </label>
+            <label>
+              <p>Bairro</p>
+              <input
+                type="text"
+                placeholder="Digite o nome de seu bairro"
+                {...register("burgh")}
+              />
+              <span>{errors.burgh?.message}</span>
+            </label>
+            <label>
+              <p>Número da Casa</p>
+              <input
+                type="number"
+                min={1}
+                placeholder="Digite o número de sua casa"
+                {...register("number")}
+              />
+              <span>{errors.number?.message}</span>
+            </label>
+            <label>
+              <p>Cidade</p>
+              <input
+                type="text"
+                placeholder="Digite o nome de sua cidade"
+                {...register("city")}
+              />
+              <span>{errors.city?.message}</span>
+            </label>
+            <label>
+              <p>Estado</p>
+              <input
+                type="text"
+                placeholder="Digite o nome do seu estado"
+                {...register("state")}
+              />
+              <span>{errors.state?.message}</span>
+            </label>
+          </form>
+        </div>
 
         <div className={styles.checkout_column}>
           <h2 className={styles.title}>Forma de Pagamento</h2>
           <div className={styles.payments_methods}>
             <div className={styles.payment}>
-              <input type="radio" name="payment" id="pix"/>
+              <input type="radio" name="payment" id="pix" />
               <label htmlFor="pix">PIX</label>
             </div>
             <div className={styles.payment}>
-              <input type="radio" name="payment" id="ticket"/>
+              <input type="radio" name="payment" id="ticket" />
               <label htmlFor="ticket">Boleto</label>
             </div>
             <div className={styles.payment}>
-              <input type="radio" name="payment" id="card"/>
+              <input type="radio" name="payment" id="card" />
               <label htmlFor="card">Cartão</label>
             </div>
           </div>
@@ -100,7 +121,12 @@ function Checkout() {
             <span>Total R${subtotal.toFixed(2)}</span>
           </div>
 
-          <button className={styles.btn_payment} onClick={handleSubmit(finalizeOrder)}>Finalizar Pedido</button>
+          <button
+            className={styles.btn_payment}
+            onClick={handleSubmit(finalizeOrder)}
+          >
+            Finalizar Pedido
+          </button>
         </div>
       </div>
     </div>
